57912a3de84de08426f87a28413e382f9d48ddf9
Sasha <faty422@mail.ru>
2023-06-27 22:01:10 +0300

    Updated math script

---
+++ docs/math.js
@@ -0,0 +1,28 @@
+var page = 1
+function openChapter(filename, n) {
+    page = n
+    fetch(filename)
+        .then((res) => res.text())
+        .then((text) => {
+            document.getElementById('content').innerHTML = marked.parse(text);
+            renderMathInElement(document.getElementById('content'),
+            {
+                delimiters: [
+                    {left: "$$", right: "$$", display: true},
+                    {left: "\\[", right: "\\]", display: true},
+                    {left: "$", right: "$", display: false},
+                    {left: "\\(", right: "\\)", display: false}
+                ]
+            });
+        })
+        .catch((e) => console.error(e));
+}
+
+function openChapterNext() {
+    page += page >= 32 ? 0 : 1
+    openChapter('math/' + page + '.MD', page)
+}
+function openChapterPrevious() {
+    page -= page <= 1 ? 0 : 1
+    openChapter('math/' + page + '.MD', page)
+}
